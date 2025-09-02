1563e6db18c7366b47ef2d3ac1f6110da41c64c1
marcos torres <marcosezequiel_torres@hotmail.com>
2025-09-02 16:48:48 -0300

    creacion procedimientos almacenados

--- ComercioInterior.sql
+++ ComercioInterior.sql
@@ -6,7 +6,8 @@ go
 create table Articulos(
 	id int identity (1,1),
 	nombre varchar(50),
-	precioUnitario decimal(10,2)
+	precioUnitario decimal(10,2),
+	estaActivo bit
 	constraint pk_articulos primary key(id)
 )
 
@@ -30,11 +31,101 @@ create table DetallesFactura(
 	idDetalle int,
 	idFactura int,
 	idArticulo int,
-	idCantidad int
+	cantidad int
 	constraint pk_detallesFactura primary key (idDetalle, idFactura),
 	constraint fk_detallesFactura_factura foreign key (idFactura)
 	references Facturas(nroFactura),
 	constraint fk_detallesFactura_articulo foreign key (idArticulo)
 	references Articulos(id)	
 )
+go
 
+CREATE PROCEDURE SP_GUARDAR_ARTICULO
+@id int,
+@nombre varchar(20),
+@precioUnitario int
+@estaActivo bit
+AS
+BEGIN 
+	IF @id = 0
+		BEGIN
+			insert into ARTICULOS (nombre, precioUnitario, estaActivo) 
+			values (@nombre,@precioUnitario,@estaActivo)	
+		END
+	ELSE
+		BEGIN
+			update ARTICULOS 
+			set nombre= @nombre, precioUnitario= @precioUnitario, estaActivo= @estaActivo 
+			where id=@id
+		END
+END
+go
+
+CREATE PROCEDURE SP_INSERTAR_DETALLE
+	@nroFactura int,
+	@idDetalle int,
+	@idArticulo int,
+	@cantidad int
+AS
+BEGIN
+	INSERT INTO DetallesFactura(idDetalle, idFactura, idArticulo, cantidad) VALUES (@idDetalle, @nroFactura, @idArticulo, @cantidad);	
+END
+go
+
+CREATE PROCEDURE SP_INSERTAR_FACTURA
+	@cliente varchar(50),
+	@fecha date,
+	@idForma int,
+	@id int output
+AS
+BEGIN
+	INSERT INTO Facturas(cliente, fecha, idForma ) VALUES (@cliente, GETDATE(), @idForma);
+	SET @id = SCOPE_IDENTITY();
+END
+GO
+
+CREATE PROCEDURE SP_RECUPERAR_FACTURA_POR_ID
+	@id int
+AS
+BEGIN
+	SELECT f.*, df.cantidad, a.*
+	  FROM Facturas f
+	  INNER JOIN DetallesFactura df ON f.nroFactura = df.idFactura
+	  INNER JOIN Articulos a ON df.idArticulo = a.id
+	  WHERE f.nroFactura = @id;
+END
+GO
+
+CREATE PROCEDURE SP_RECUPERAR_FACTURAS
+AS
+BEGIN
+	SELECT f.*, df.cantidad, a.*
+	  FROM Facturas f
+	  INNER JOIN DetallesFactura df ON df.idDetalle =f.nroFactura
+	  INNER JOIN Articulos a ON a.id = df.idArticulo
+	  ORDER BY f.nroFactura;
+END
+GO
+
+CREATE PROCEDURE SP_RECUPERAR_ARTICULOS_POR_CODIGO
+	@id int
+AS
+BEGIN
+	SELECT * FROM Articulos WHERE id = @id;
+END
+GO
+
+CREATE PROCEDURE SP_RECUPERAR_ARTICULOS
+AS
+BEGIN
+	SELECT * FROM Articulos
+END
+GO
+
+CREATE PROCEDURE SP_REGISTRAR_BAJA_ARTICULO
+	@id int 
+AS
+BEGIN
+	UPDATE Articulos SET estaActivo = 0 WHERE id = @id;
+END
+GO
